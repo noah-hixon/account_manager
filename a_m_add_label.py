7edf78e752dc92eaa4969921be10edc1ec42ca21
noah-hixon <57816980+noah-hixon@users.noreply.github.com>
2019-12-18 23:10:32 -0500

    Polished labels

--- a_m_add_label.py
+++ a_m_add_label.py
@@ -15,27 +15,12 @@ root = tk.Tk()
 root.title("Account Manager")
 root.geometry("+400+200")
 users = []
-global count
+
+
+
 #################
 
 
-# takes userinfo from entry boxes and displays it
-# def add_info(result_name, result_user, result_pass):
-
-#     result_name = name_e.get()
-#     new_name = name_l.cget("text") + result_name
-#     name.set(new_name + "\n")
-
-#     result_user = user_e.get()
-#     new_user = user_l.cget("text") + result_user
-#     user.set(new_user + "\n")
-
-#     result_pass = pass_e.get()
-#     new_pass = pass_l.cget("text") + result_pass
-#     password.set(new_pass + "\n")
-
-    # delete_button = tk.Button(root, text = "X", fg = "red", command = delete_entry)
-    # delete_button.grid(row= 4, column = 3)
 
 
 def add_info(name, user, password, count):
@@ -47,42 +32,28 @@ def add_info(name, user, password, count):
     user_display.grid(row = (4 + count), column = 1)
     pass_display.grid(row = (4 + count), column = 2)
 
-    print("name,", name)
-# takes the userinfo from get_info_from_file and displays it
-# def add_from_file(result_name, result_user, result_pass):
-#     new_name = name_l.cget("text") + result_name
-#     name.set(new_name + "\n")
-
-#     new_user = user_l.cget("text") + result_user
-#     user.set(new_user + "\n")
-
-#     new_pass = pass_l.cget("text") + result_pass
-#     password.set(new_pass + "\n")
-
-    # delete_button = tk.Button(root, text = "X", fg = "red", command = delete_entry)
-    # delete_button.grid(row= 4, column = 3)
-
-
-def add_from_file(name, user, password):
+def add_new():
     name_display = tk.Label(root, text = name)
     user_display = tk.Label(root, text = user)
     pass_display = tk.Label(root, text = password)
 
-    name_display.grid(row = 4, column = 0)
-    user_display.grid(row = 4, column = 1)
-    pass_display.grid(row = 4, column = 2)
+    name_display.grid(row = (4 + count), column = 0)
+    user_display.grid(row = (4 + count), column = 1)
+    pass_display.grid(row = (4 + count), column = 2)
+
+
+
 
 
 # on button click, a window pops up asking
-# the user which entry they want to delete
+# the user which entry they want to delete ***DOESN'T DO ANYTHING YET***
 def delete_entry():
     user_info = open("userinfo.txt", "r")
     user_list = []
+    
     count = 0
     for line in user_info:
-        
         entry = str(line) + str(count)
-        
         user_list.append(entry)
         count += 1
     print(user_list)
@@ -92,9 +63,7 @@ def delete_entry():
     entry_show = tk.Label(message, text = user_list)
     entry_show.pack()
     
-    
     # make a counter, make each line of the file a variable, add to list?, add to label
-    
     message.mainloop()
     # tk.messagebox.showinfo("Delete Entry", "Which entry to you want to delete?")
 	
@@ -107,32 +76,31 @@ def get_info_from_file():
     user_info.readline()
     count = 0
     for line in user_info:
+        print("count", count)
         alist = line.split(',')
-        
         name = alist[0]
         user = alist[1]
         password = alist[2]
         add_info(name, user, password, count)
         count +=1
-        
+
 
 # when user clicks "Submit", userinfo is written to a text file and
 # the userinfo entered is displayed
 def on_submit():
-
+    
     user_info = open("userinfo.txt","a")
     name = name_e.get()
     user = user_e.get()
     password = pass_e.get()
-    #add_info(name, user, password)
     user_info.write(name + "," + user + "," + password + ",\n" )
-    # add_info(name, user, password)	
-
+    # add_info(name, user, password, count)
+    # print("count:", count)
+    get_info_from_file()
     name_e.delete(0, "end")
     user_e.delete(0, "end")
     pass_e.delete(0, "end")
     
-
     tk.messagebox.showinfo('Success!','Successfully Added, \n' + 'Name: ' + name + '\nUser: ' + user + '\nPassword: ' + password)
 	
 
